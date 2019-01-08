9bf3c594bb5f95fec26ce4d6dc5d187dfe44ea20
brownsoo han <hansune@me.com>
2019-01-08 15:35:01 +0900

    fixed Casting MvsSharedViewController

--- QuickIosComponent/Classes/mvs/CreateStateShared.swift
+++ QuickIosComponent/Classes/mvs/CreateStateShared.swift
@@ -11,11 +11,11 @@ import ReSwift
 import ReSwiftConsumer
 
 public extension MvsSharedViewController {
-    public static func newStateSharedInstance<State: StateType & Equatable, I: MvsInteractor<State>>(
+    public static func newStateSharedInstance<State: StateType & Equatable, I: MvsInteractor<State>, T>(
         store: Store<State>,
         consumer: StateConsumer<State>,
-        interactor: I? = nil) -> MvsSharedViewController<State, I> {
-        let me = MvsSharedViewController<State, I>()
+        interactor: I? = nil) -> T where T: MvsSharedViewController<State, I> {
+        let me: T = self.init() as! T
         me.bind(store: store, consumer: consumer)
         me.interactor = interactor
         return me
