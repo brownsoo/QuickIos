883440a1cd7a2b82901b2b02badd6e79df59e588
brownsoo han <hansune@me.com>
2019-03-25 18:28:56 +0900

    remove PaddingView

--- QuickIosComponent/Classes/component/JustView.swift
+++ QuickIosComponent/Classes/component/JustView.swift
@@ -20,46 +20,6 @@ open class JustView: UIView {
     }
 }
 
-open class PaddingView: JustView {
-    public let contentView = UIView()
-    private var contentConstraints = [String: NSLayoutConstraint]()
-    public var padding: UIEdgeInsets = UIEdgeInsets() {
-        didSet {
-            contentConstraints["leading"]?.constant = padding.left
-            contentConstraints["trailing"]?.constant = -padding.right
-            contentConstraints["top"]?.constant = padding.top
-            contentConstraints["bottom"]?.constant = -padding.bottom
-            setNeedsUpdateConstraints()
-        }
-    }
-    open override func onInit() {
-        addSubview(contentView)
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        let leading = contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding.left)
-        leading.priority = .required
-        leading.isActive = true
-        let trailing = contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding.right)
-        trailing.priority = .required
-        trailing.isActive = true
-        let top = contentView.topAnchor.constraint(equalTo: self.topAnchor, constant: padding.top)
-        top.priority = .required
-        top.isActive = true
-        let bottom = contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -padding.bottom)
-        bottom.priority = .required
-        bottom.isActive = true
-
-        contentConstraints["leading"] = leading
-        contentConstraints["trailing"] = trailing
-        contentConstraints["top"] = top
-        contentConstraints["bottom"] = bottom
-    }
-
-    open func addContentView(_ v: UIView) {
-        contentView.addSubview(v)
-    }
-
-}
-
 open class JustButton: UIButton {
     public convenience init() {
         self.init(type: .custom)
